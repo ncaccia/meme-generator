db6a5f0226bb667979ac630458cc88809fab5453
Nica <ncacciavillani@gmail.com>
2024-10-22 19:03:33 +0200

    uplod component

---
+++ src/app/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+// import Image from "next/image";
+import { IKImage, IKUpload, ImageKitProvider } from "imagekitio-next";
+import { useState } from "react";
+
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
+const authenticator = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth");
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error: unknown) {
+    let errorMessage = "Authentication request failed: ";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
+  }
+};
+
+export default function Home() {
+  const [filePath, setFilePath] = useState("")
+  return (
+    <div className="">
+      <ImageKitProvider
+        publicKey={publicKey}
+        urlEndpoint={urlEndpoint}
+        authenticator={authenticator}
+      >
+
+        {filePath && // only display the image if filePath is truththly) 
+          <IKImage
+            path={filePath}
+            width={500}
+            height={400}
+            transformation={[
+              {
+                raw: "l-text,i-hello world,fs-50,l-end",
+              },
+            ]}
+            alt="baloon graving meme"
+          />
+        }
+
+        <div>
+          <h2>File upload</h2>
+          <IKUpload
+            fileName="test-upload.png"
+            onError={(err) => {
+              console.log("Error:", err);
+
+            }}
+            onSuccess={(res) => {
+              console.log("Success: ", res);
+              setFilePath(res.filePath)
+            }}
+          />
+        </div>
+      </ImageKitProvider>
+    </div>
+  );
+}
